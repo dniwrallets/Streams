f9ba5ecc398d705136653522dd2883c332f2394c
John <dniwrallets@gmail.com>
2020-05-06 13:21:07 -0400

    Updated naming

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="ui secondary pointing menu">
       <Link to="/" className="item">
-        JS-Stream
+        Streams
       </Link>
       <div className="right menu">
         <Link to="/" className="item">
